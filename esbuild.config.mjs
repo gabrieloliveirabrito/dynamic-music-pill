70c020afa9fdcbe9039b7f12d5e3b4f1d450e312
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-16 21:26:44 -0300

    feat: Add release minify

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,20 +1,23 @@
 import esbuild from "esbuild";
 
 const watch = process.argv.includes("--watch");
+const release = process.argv.includes("--release");
 
 const ctx = await esbuild.context({
     entryPoints: ["src/extension.ts"],
     bundle: true,
+    minify: release,
     outfile: "dist/extension.js",
     format: "esm",
     target: "es2017",
     sourcemap: true,
     platform: "neutral",
+    logLevel: "info",
     external: [
         "resource://*",
         "gi://*"
     ]
-});
+}); 
 
 if (watch) {
     await ctx.watch();
