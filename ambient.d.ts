3cc726d962b447e0936799a4b31fecbb97f45994
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-18 17:57:53 -0300

    fix: Import @girs modules

--- ambient.d.ts
+++ ambient.d.ts
@@ -1,3 +1,12 @@
+/// <reference types="@girs/gnome-shell" />
+
 // ambient.d.ts
 import '@girs/gnome-shell/ambient';
+import '@girs/clutter-18/ambient';
+import '@girs/gdkpixbuf-2.0/ambient';
+import '@girs/gio-2.0/ambient';
+import '@girs/glib-2.0/ambient';
+import '@girs/gobject-2.0/ambient';
+import '@girs/st-18/ambient';
+
 declare module 'resource://*';
