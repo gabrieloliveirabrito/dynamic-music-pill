066710ea822fc5adbf4eea5a55f38a91e60d5184
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-17 22:08:10 -0300

    feat: Add logObject and logTrace

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,6 @@
-const PREFIX = "[DMP]";
+import { LogConstants } from "@/constants";
+
+const PREFIX = LogConstants.LOG_PREFIX;
 
 export function logInfo(message: string): void {
     console.log(`${PREFIX} ${message}`);
@@ -18,4 +20,29 @@ export function logError(message: unknown): void {
     if (message instanceof Error) {
         console.error(`${PREFIX} Stack trace: ${message.stack}`);
     }
+}
+
+export function logTrace(message: string) {
+    console.trace(`${PREFIX} ${message}`);
+}
+
+export function logObject(object: any | null | undefined): void {
+    if (object === null) {
+        logTrace("Object is null");
+        return;
+    }
+
+    if (object === undefined) {
+        logTrace("Object is undefined");
+        return;
+    }
+
+    let inspected = JSON.stringify(object, (key, value) => {
+        if (value && value.deep_unpack) {
+            return value.deep_unpack();
+        }
+
+        return value;
+    })
+    logInfo(`Inspecting object ${inspected}`);
 }
