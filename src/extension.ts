a41b97e374cf234b15f4b3785d1c01bbb42ab66f
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-17 22:09:51 -0300

    chore: First MPRIS callbacks

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
-import { Extension } from "@girs/gnome-shell/extensions/extension"
+import { Extension } from "@girs/gnome-shell/src/extensions/extension"
 import { logDebug, logInfo, logWarning, logError } from "./utils/log";
+import { isDevelopment } from "./utils/development";
+import { IMPrisProvider } from "./interfaces/impris-provider";
+import { createMockMPRISProvider } from "./providers/mock";
+import { createMPRISProvider } from "./providers/mpris-provider";
 
-let instance : DynamicMusicPillExtension | null = null;
+let instance: DynamicMusicPillExtension | null = null;
 
-export function getInstance() : DynamicMusicPillExtension | null {
+export function getInstance(): DynamicMusicPillExtension | null {
     if (instance === null) {
         throw new Error("getInstance called before instance was created");
     }
@@ -12,17 +16,28 @@ export function getInstance() : DynamicMusicPillExtension | null {
 
 export default class DynamicMusicPillExtension {
     extension: Extension;
+    provider: IMPrisProvider;
 
     constructor(extension: Extension) {
         this.extension = extension;
         instance = this;
+
+        this.provider = createMPRISProvider();
+        this.provider.addCallback("first", (track) => {
+            logInfo(`Chegou aqui ${JSON.stringify(track)}`);
+        })
     }
 
-    enable() { 
+    enable() {
         logInfo("Extension enabled.");
+        logInfo(isDevelopment() ? "Is Dev" : "Is Not Dev");
+        
+        this.provider.start();
     }
 
     disable() {
+        this.provider.stop();
+        
         logWarning("Extension disabled.");
     }
 }
