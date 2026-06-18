c2e9310c2bb731c08fb9fa1f86bec787b05044dd
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-17 22:06:12 -0300

    chore: Export DBUS parent

--- runner.sh
+++ runner.sh
@@ -16,7 +16,7 @@ for ext in "${KEEP_EXTENSIONS[@]}"; do
     gnome-extensions enable "$ext"
 done
 
-#export DBUS_PARENT='unix:path=/run/user/1000/bus'
+export DBUS_PARENT='unix:path=/run/user/1000/bus'
 export DEV_MODE=1
 
 gnome-shell --wayland --devkit
