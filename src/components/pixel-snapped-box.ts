e678d111b9937ee8f8a646e83b8d2527f67521fa
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2026-06-24 20:37:28 -0300

    feat: Update components

--- src/components/pixel-snapped-box.ts
+++ src/components/pixel-snapped-box.ts
@@ -1,17 +1,20 @@
+import { BoxLayoutProps } from "@/types/shell-types"
 import Clutter from "@girs/clutter-18"
 import { GObject } from "@girs/gobject-2.0/gobject-2.0"
 import { St } from "@girs/st-18/st-18"
 
-export const PixelSnappedBox = GObject.registerClass(
-    class PixelSnappedBox extends St.BoxLayout {
-        vfunc_allocate(box: Clutter.ActorBox): void {
-            box.x1 = Math.round(box.x1);
-            box.x2 = Math.round(box.x2);
-            box.y1 = Math.round(box.y1);
-            box.y2 = Math.round(box.y2);
 
-            super.vfunc_allocate(box);
-        }
+export class PixelSnappedBox extends St.BoxLayout {
+    static {
+        GObject.registerClass(this);
     }
-)
 
+    vfunc_allocate(box: Clutter.ActorBox): void {
+        box.x1 = Math.round(box.x1);
+        box.x2 = Math.round(box.x2);
+        box.y1 = Math.round(box.y1);
+        box.y2 = Math.round(box.y2);
+
+        super.vfunc_allocate(box);
+    }
+}
